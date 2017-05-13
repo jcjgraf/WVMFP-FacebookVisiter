3be13137aabcbde14517a7707305ae96f6f5997f
Jean-Claude Graf <jeanggi90@gmail.com>
2017-05-13 23:35:11 +0200

    Version 1.0

--- wvmp.py
+++ wvmp.py
@@ -4,12 +4,30 @@
 # LIBRARIES #
 #############
 
-import mechanize
-import time
-from lxml import html
-import json
+import mechanize    # Login to facebook
+from lxml import html   # Analyse the source code with xpath
 
-import demjson
+
+##################
+# DATA STRUCTURE #
+##################
+
+class User:
+    """
+        Holds data about the user
+    """
+    def getLogin():
+        login = ""
+
+        with open('login.txt', 'r') as loginFile:
+            login = loginFile.read()
+            loginFile.close()
+
+        return login.split(":")
+
+    userID = ""
+    email = getLogin()[0]
+    passwd = getLogin()[1]
 
 
 #############
@@ -17,6 +35,9 @@ import demjson
 #############
 
 def setupBrowser():
+    """
+        Setup the browser which we use for login to facebook and scraping the source code
+    """
     br = mechanize.Browser()
     cj = mechanize.LWPCookieJar()
     br.set_cookiejar(cj)
@@ -29,6 +50,9 @@ def setupBrowser():
     return br
 
 def login(username,password):
+    """
+        Login to facebook with the email and password given in the argument
+    """
     print("[-] LOGIN")
     print("[+] username : " + username)
     print("[+] password : " + password)
@@ -47,6 +71,9 @@ def login(username,password):
     return br
 
 def logout(br):
+    """
+        For a proppre sign out of facebook
+    """
     print("[-] LOGOUT")
     br.open('https://m.facebook.com/')
 
@@ -54,38 +81,67 @@ def logout(br):
         br.open('https://m.facebook.com' + link.url)
 
 def getUserID(scrapedCode):
-    print("Getting the user ID")
+    """
+        Scrapes source code and gets the userID from it
+    """
+    print("[+] Getting the user ID")
     tree = html.fromstring(scrapedCode)
 
     idValue = tree.xpath('//img[1]/@id')[1] #Weirdly it returns a list with two elements, the second one is the right ID value
 
-    userID = idValue[19:] # Strip the beginning and get the id
-    return userID
+    User.userID = idValue[19:] # Strip the beginning and get the userID
 
 def getVisiterID(scrapedCode):
-    print("Getting the visiters ID")
-    # print(scrapedCode)
+    """
+        Here we get all script tag sections from the source code, analyse them and select the right one, strip all unnescessary stuff, converte the js object to a json  and save it to a file
+    """
+
+    print("[+] Getting the visiter IDs")
     tree = html.fromstring(scrapedCode)
 
     scriptTags = tree.xpath('//script')
     scriptTag = ""
 
+    # Iterate over all script tag section and look for the one containing the right keyword
     for s in scriptTags:
         if "InitialChatFriendsList" in html.tostring(s):
             print("Found")
             scriptTag = html.tostring(s)
             break
 
-    visitersObject = scriptTag.split('shortProfiles:', 1)[-1].rsplit('nearby:', 1)[0][19:-1]
-
-    # Because it is a JS object, the quotes arround the keys are missing. So we gotta add them
-    
+    visitersObject = scriptTag.split('shortProfiles:', 1)[-1].rsplit('nearby:', 1)[0][:-1]
 
     with open('visiters.json', 'w') as visiters:
-        visiters.write(demjson.decode(visitersObject))
+        visiters.write(jsObjectToJson(visitersObject))
+        # visiters.write(visitersObject)
 
+def jsObjectToJson(objectString):
+    """
+        The retrieved data is in js object format. This means that there are no quotes arround the keys, so we have to add them in otder to get a propre json file
+    """
+    jsonString = ""
+    lastCutPos = 0
+    x = 0
+
+    for letter in objectString:
+        if (letter == "{" or letter == ",") and not objectString[x + 1] == '"':
+            jsonString += objectString[lastCutPos : x + 1] + '"'
+            lastCutPos = x + 1
+        elif letter == ":" and not (objectString[x - 1] == '"' or objectString[x + 1] == "/" or objectString[x + 1] == "{"):
+            jsonString += objectString[lastCutPos : x ] + '":'
+            lastCutPos = x + 1
+
+        x += 1
+
+    jsonString += objectString[lastCutPos : ]
+
+    return jsonString
 
 def writeToTestfile(textToSave):
+    """
+        This function is mainly used for testing and debuggind purpose
+    """
+
     print("[-] WRITE TO FILE")
     with open('test.html', 'w') as file:
         file.write(textToSave)
@@ -97,15 +153,15 @@ def writeToTestfile(textToSave):
 
 if __name__ == '__main__':
 
-    br = login("jeanggi90@gmail.com", "Dijcfa90")
+    br = login(User.email, User.passwd)
 
+    # Scrape the main page in order to get userID
     response = br.open('https://facebook.com/')
-    userID = getUserID(str(response.read()))
+    getUserID(str(response.read()))
 
-    response = br.open('https://facebook.com/' + userID)
-    # writeToTestfile(response.read())
-    visiterID = getVisiterID(str(response.read()))
-    
+    # Scrape the profile page in order to get the js object containing the data about the visiters
+    response = br.open('https://facebook.com/' + User.userID)
+    getVisiterID(str(response.read()))
 
     logout(br)
 
